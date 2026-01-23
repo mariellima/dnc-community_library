4b3e6474d0219c3e756bc513ee897d976e7d3d26
mariellima <marielgomes1@outlook.com>
2026-01-23 15:30:56 -0300

    feat: service, controller and routes

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 import express from 'express'
+import userRouters from './src/routes/user.routes.js'
 const app = express();
 
 app.use(express.json())
+app.use(userRouters)
 
-const users = []
 
-// METHOD => GET, POST, PUT, DELETE
-// NAME => / - sempre no plural
-// Callback functions => Onde executamos o backend (lógica, regra de negócio)
-
-app.post('/users', (req, res) => {
-  const body = req.body
-  users.push(body)
-  res.status(201).send("Usuário criado com sucesso!")
-})
-
-app.get("/users", (req, res) => {
-  res.send({message: "Esse são os users", users})
-})
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
